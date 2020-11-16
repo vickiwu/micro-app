3ed94dde74fc2269a772035886eb035bffd430f1
vickiwu <794431483@qq.com>
2020-11-16 11:15:19 +0800

    删除

--- micro-front-template/micro-app-main/src/micro/es/sandbox/common.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * @author Kuitos
- * @since 2020-04-13
- */
-export declare function getTargetValue(target: any, value: any): any;
-export declare function getProxyPropertyGetter(proxy: WindowProxy, property: PropertyKey): any;
-export declare function setProxyPropertyGetter(proxy: WindowProxy, property: PropertyKey, getter: () => any): void;
