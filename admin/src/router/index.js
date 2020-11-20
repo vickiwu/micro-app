df8c6138fb54fbd9fb43c279e5a316e3d9685958
vickiwu <794431483@qq.com>
2020-11-20 10:10:05 +0800

    修改admin项目路由为history

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -59,14 +59,13 @@ export const constantRoutes = [
 ]
 
 const createRouter = () => new Router({
-  // mode: 'history', // require service support
+  mode: 'history', // require service support
   scrollBehavior: () => ({ y: 0 }),
   routes: constantRoutes
 })
 
 const router = createRouter()
 
-// Detail see: https://github.com/vuejs/vue-router/issues/1234#issuecomment-357941465
 export function resetRouter() {
   const newRouter = createRouter()
   router.matcher = newRouter.matcher // 重置路由
